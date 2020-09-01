58b384e84ce6206de1923f660677a556ba7d14ff
2910030 <barti20000@gmail.com>
2020-09-01 20:35:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def mtx_multiply(a, b):
+    m = len(a)
+    n = len(b[1])
+
+    p = n
+    C = np.zeros((m,n))
+    for i in range(0, m):
+        for j in range(0, p):
+            suma = 0
+            for k in range(0, n):
+                suma = suma + A[i][k]*B[k][j]
+            C[i][j] = suma
+
+    return C
+
+A = [[1, 2, 3],
+     [3, 2, 1],
+     [2, 1, 3]]
+B = [[3, 2],
+    [1, 1],
+    [4, 1]]
+C=mtx_multiply(A, B)
+print(C)
+
